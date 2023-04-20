e4622fdf92b6d882a8c39393c2e6ca4a753e2538
RitchielRez <ritchielrez@gmail.com>
2023-04-20 18:30:16 +0600

    inital commit

---
+++ include/projectGenerate.hpp
@@ -0,0 +1,47 @@
+#ifndef PROJECT_GENERATE_H
+#define PROJECT_GENERATE_H
+
+#include <cstdlib>
+#include <cstring>
+
+#ifdef WINDOWS
+#include <direct.h>
+#include <windows.h>
+#define GetCurrentDir _getcwd
+#else
+#include <signal.h>
+#include <unistd.h>
+#define getCurrentDir getcwd
+#endif
+
+#define STRING_SIZE_NAME_MAX 256
+
+namespace projectGenerate
+{
+inline int appendPath(char *dest, const char *src)
+{
+    int destlen = strlen(dest) + strlen(src) + 1;
+
+    if (destlen <= STRING_SIZE_NAME_MAX)
+    {
+        strcat(dest, "/");
+        strcat(dest, src);
+
+        return 0;
+    }
+    return 1;
+}
+
+inline int makeDir(const char *path)
+{
+    char cmd[STRING_SIZE_NAME_MAX] = "mkdir ";
+    strcat(cmd, path);
+
+    if (system(cmd) == -1)
+        return 1;
+
+    return 0;
+}
+} // namespace projectGenerate
+
+#endif // PROJECT_GENERATE_H
